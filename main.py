967b02e21113b24e2ce9283e5c4ec934437c5ac9
pchudzik <01178632@pw.edu.pl>
2024-01-18 03:24:30 +0100

    próba dodania drobnych usprawnień (print_frame)

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ import matplotlib as mpl
 
 h = 1  # spatial step width
 t = 1  # time step width
-dimx = 101  # width of the simulation domain
-dimy = 101  # height of the simulation domain
-cellsize = 5  # display size of a cell in pixel
+dimx = 1001  # width of the simulation domain
+dimy = 1001  # height of the simulation domain
+cellsize = 0.5  # display size of a cell in pixel
 offset = 8
-deriv_coffs_space = np.array(savgol_coeffs(offset * 2 + 1, offset * 2, deriv=2, use='dot'), dtype=np.float64)
+deriv_coffs_space = (np.array(savgol_coeffs(offset * 2 + 1, offset * 2, deriv=2, use='dot'), dtype=np.float64))[offset:]
 deriv_coffs_time = np.array(savgol_coeffs(3, 2, deriv=2, use='dot'), dtype=np.float64)
 cmap = np.array([np.array(mpl.cm.inferno(i)[:3]) * 256 for i in np.linspace(0, 1, 256)], dtype=np.uint8)
 pass
+thread_block = (8, 4)
 
 
 def init_simulation():
     u = np.zeros((3, dimx + offset * 2 + 2, dimy + offset * 2 + 2), dtype=np.float64)
-    # u[0, dimx // 2, dimy // 2] = 1000
-    u[0, dimx // 2, :] = 1000
+    u[0, u.shape[1] // 2, u.shape[2] // 2] = 1000
+    # u[0, dimx // 2, :] = 1000
     # u[0, 9, 9] = 10
     # The three dimensional simulation grid u[time, dimx, dimy]
 
@@ -32,9 +33,9 @@ def init_simulation():
     # wave propagation velocities of the entire simulation domain
 
     alpha[:, :] = ((c * t) / h) ** 2  # will be set to a constant value of tau
-    alpha[2 * dimx // 3:2 * dimx // 3 + 10, 0:30] = 0
-    alpha[2 * dimx // 3:2 * dimx // 3 + 10, 30:40] = 0
-    alpha[2 * dimx // 3:2 * dimx // 3 + 10, 60:70] = 0
+    # alpha[2 * dimx // 3:2 * dimx // 3 + 10, 0:30] = 0
+    # alpha[2 * dimx // 3:2 * dimx // 3 + 10, 30:40] = 0
+    # alpha[2 * dimx // 3:2 * dimx // 3 + 10, 60:70] = 0
 
     return u, alpha
 
@@ -120,48 +121,126 @@ def update(u, result, alpha, deriv_coffs):
         #     pass
 
 
-@cuda.jit(f'void(float64[:,:,:], float64[:,:,:], float64[:,:], float64[:], float64[:])')
-def update2(u, result, alpha, deriv_coffs_space, deriv_coffs_time):
+@cuda.jit(f'void(float64[:,:,:], float64[:,:], float64[:], float64[:])')
+def generate_new_frame(u, alpha, deriv_coffs_space, deriv_coffs_time):
     x, y = cuda.grid(2)
+    idx = cuda.threadIdx.x
+    idy = cuda.threadIdx.y
+
+    u_shared = cuda.shared.array(thread_block, dtype=np.float64)
 
     if x < u.shape[1] and y < u.shape[2]:
-        # k = 4
-        # while k > 0:
-        #     u[k, x, y] = u[k - 1, x, y]
-        #     k -= 1
 
-        # cuda.syncthreads()
+        # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #
+        # $$$ Generacja następnej klatki $$$ #
+        # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #
 
         if 0 < x < u.shape[1] - 1 and 0 < y < u.shape[2] - 1:
+            x_plus = True
+            x_minus = True
+            y_plus = True
+            y_minus = True
+
             temp = 0
             i = 0
-            while i < deriv_coffs_space.shape[0]:
-                if 0 < i + x - offset < u.shape[1] - 1:
-                    temp += u[1, i + x - offset, y] * deriv_coffs_space[i]
-                i += 1
 
-            i = 0
             while i < deriv_coffs_space.shape[0]:
-                if 0 < i + y - offset < u.shape[2] - 1:
-                    temp += u[1, x, i + y - offset] * deriv_coffs_space[i]
+                if u.shape[1] - 1 > x + i > 0 and x_plus:
+                    if alpha[x + i, y] == 0:
+                        x_plus = False
+                    else:
+                        temp += u[1, x + i, y] * deriv_coffs_space[i]
+
+                if u.shape[1] - 1 > x - i > 0 != i and x_minus:
+                    if alpha[x - i, y] == 0:
+                        x_minus = False
+                    else:
+                        temp += u[1, x - i, y] * deriv_coffs_space[i]
+
+                if u.shape[1] - 1 > y + i > 0 and y_plus:
+                    if alpha[x, y + i] == 0:
+                        y_plus = False
+                    else:
+                        temp += u[1, x, y + i] * deriv_coffs_space[i]
+
+                if u.shape[1] - 1 > y - i > 0 != i and y_minus:
+                    if alpha[x, y - i] == 0:
+                        y_minus = False
+                    else:
+                        temp += u[1, x, y - i] * deriv_coffs_space[i]
+
                 i += 1
+                # x_plus = True
+                # x_minus = True
+                # y_plus = True
+                # y_minus = True
 
             temp *= (alpha[x, y] ** 2)
-            result[0, x, y] = temp
 
             i = 1
             while i < deriv_coffs_time.shape[0]:
                 temp -= u[i, x, y] * deriv_coffs_time[i]
                 i += 1
 
-            # u[0, x, y] = temp / deriv_coffs_time[0]
-            if (10 ** (-310)) >= temp / deriv_coffs_time[0] * 0.9 >= -(10 ** (-310)):
+            if (10 ** (-310)) >= temp / deriv_coffs_time[0] >= -(10 ** (-310)):
                 u[0, x, y] = 0
+                u_shared[idx, idy] = 0
             else:
-                u[0, x, y] = (temp / deriv_coffs_time[0]) * 0.9
+                u[0, x, y] = (temp / deriv_coffs_time[0])
+                u_shared[idx, idy] = (temp / deriv_coffs_time[0])
 
-        elif (x == 0 or x == u.shape[1] - 1) ^ (y == 0 or y == u.shape[2] - 1):
-            pass
+        # elif (x == 0 or x == u.shape[1] - 1) ^ (y == 0 or y == u.shape[2] - 1):
+        #     pass
+
+
+@cuda.jit('void(float64[:,:,:], uint8[:,:,:], uint8[:,:], float64, float64[:,:])')
+def print_frame(u, pixeldata, cmap, max_val_data, debug):
+    x, y = cuda.grid(2)
+
+    if x < pixeldata.shape[0] and y < pixeldata.shape[1]:
+        x_low = x * u.shape[1] / pixeldata.shape[0]
+        x_high = (x + 1) * u.shape[1] / pixeldata.shape[0]
+        y_low = y * u.shape[2] / pixeldata.shape[1]
+        y_high = (y + 1) * u.shape[2] / pixeldata.shape[1]
+
+        pixel_value = 0
+
+        ix = x_low
+        while ix < x_high:
+            if x_low % 1 != 0:
+                jx = 1 - (x_low % 1)
+            elif ix < x_high and ix + 1 < x_high:
+                jx = 1
+            else:
+                jx = x_high - ix
+
+            iy = y_low
+            while iy < y_high:
+                if y_low % 1 != 0:
+                    jy = 1 - (y_low % 1)
+                elif iy < y_high and iy + 1 < y_high:
+                    jy = 1
+                else:
+                    jy = y_high - iy
+
+                    pixel_value += jx * jy * u[0, int(ix // 1), int(iy // 1)]
+
+                iy += jy
+            ix += jx
+
+        pixel_value /= (u.shape[1] / pixeldata.shape[0]) * (u.shape[2] / pixeldata.shape[1])
+        debug[x, y] = pixel_value
+
+        i = 0
+        while i < 3:
+            if max_val_data != 0:
+                pixeldata[x, y, i] = cmap[int(pixel_value / max_val_data * 127) + 127, i]
+
+            else:
+                pixeldata[x, y, i] = cmap[127, i]
+            i += 1
+
+    pass
 
 
 @cuda.jit(f'void(float64[:,:,:], float64, uint8[:,:,:], uint8[:,:])')
@@ -199,7 +278,7 @@ def main():
     cmap_mem = cuda.to_device(cmap)
     deriv_coffs_spc_mem = cuda.to_device(deriv_coffs_space)
     deriv_coffs_tim_mem = cuda.to_device(deriv_coffs_time)
-    pixeldata = np.zeros((dimx, dimy, 3), dtype=np.uint8)
+    pixeldata = np.zeros((u.shape[1] - 2, u.shape[2] - 2, 3), dtype=np.uint8)
 
     while True:
         u = np.append(np.zeros((1, u.shape[1], u.shape[2]), dtype=np.float64), u[:-1], axis=0)
@@ -208,20 +287,25 @@ def main():
                 pygame.quit()
                 return
 
-        place_raindrops(u)
-        result = cuda.device_array((u.shape[0], u.shape[1], u.shape[2]), dtype=np.float64)
+        # place_raindrops(u)
+        debug = cuda.device_array((pixeldata.shape[0], pixeldata.shape[1]), dtype=np.float64)
 
         u = cuda.to_device(u)
         pixeldata = cuda.to_device(pixeldata)
 
-        thread_block = (8, 4)
-        block_grid = (int(np.ceil(u.shape[1] / thread_block[0])), int(np.ceil(u.shape[2] / thread_block[1])))
+        block_grid_1 = (int(np.ceil(u.shape[1] / thread_block[0])), int(np.ceil(u.shape[2] / thread_block[1])))
+        block_grid_2 = (int(np.ceil(pixeldata.shape[0] / thread_block[0])),
+                        int(np.ceil(pixeldata.shape[1] / thread_block[1])))
 
-        update2[block_grid, thread_block](u, result, alpha_mem, deriv_coffs_spc_mem, deriv_coffs_tim_mem)
+        generate_new_frame[block_grid_1, thread_block](u, alpha_mem, deriv_coffs_spc_mem, deriv_coffs_tim_mem)
+
         u = u.copy_to_host()
 
-        use_color_map[block_grid, thread_block](cuda.to_device(u), max(np.abs(u.max()), np.abs(u.min())),
-                                                pixeldata, cmap_mem)
+        # print_frame[block_grid_2, thread_block](cuda.to_device(u), pixeldata, cmap,
+        #                                         max(np.abs(u.max()), np.abs(u.min())), debug)
+
+        use_color_map[block_grid_1, thread_block](cuda.to_device(u), max(np.abs(u.max()), np.abs(u.min())),
+                                                  pixeldata, cmap_mem)
         pixeldata = pixeldata.copy_to_host()
 
         # pixeldata[1:dimx, 1:dimy, 0] = np.clip(u[0, 1:dimx, 1:dimy] + 128, -255, 255)
